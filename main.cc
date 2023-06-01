231e93f09d2011da6f157d482e9019ba84508eca
lukascodispoti <lukas.codispoti@gmail.com>
2023-06-01 14:47:00 +0200

    control grid size via command line and a bit of cleanup

--- main.cc
+++ main.cc
@@ -32,35 +32,47 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int M = 1024;
-
-    hsize_t Nloc = M / size;
-    hsize_t offset = rank * Nloc;
-    rank == size - 1 ? Nloc += M % size : Nloc;
-    printf("rank %d, Nloc %" PRIdHSIZE " offset %" PRIdHSIZE "\n", rank, Nloc,
-           offset);
-
-    /* get input filename and datatset to read from the command line */
+    /* get command line arguments */
+    int M = 1024;
     char inputfile[100], inputdset[100];
     bool restart = false;
     char restartfile[100], restartdset[100] = "phi";
     int method = 2;
-    if (argc != 3 && argc != 4 && argc != 5 && argc != 6) {
-        printf(
-            "Usage: %s <inputfile> <dataset> [<method: 0 jacobi, 1 "
-            "gauss-seidel, 2 sor> <restartfile> <restartdset>]\n",
-            argv[0]);
-        exit(1);
+    if (argc < 3 || argc > 7) {
+        if (!rank)
+            printf(
+                "Usage: %s <inputfile> <dataset> [<gridsize> <method: 0 "
+                "jacobi, 1 "
+                "gauss-seidel, 2 sor> <restartfile> <restartdset>]\n",
+                argv[0]);
+        MPI_Finalize();
     }
     strcpy(inputfile, argv[1]);
     strcpy(inputdset, argv[2]);
-    if (argc > 3) method = atoi(argv[3]);
-    if (argc > 4) {
+    if (argc > 3) M = atoi(argv[3]);
+    if (argc > 4) method = atoi(argv[4]);
+    if (argc > 5) {
         restart = true;
-        strcpy(restartfile, argv[4]);
+        strcpy(restartfile, argv[5]);
     }
-    if (argc > 5) strcpy(restartdset, argv[5]);
+    if (argc > 6) strcpy(restartdset, argv[6]);
 
+    if (!rank) {
+        printf("inputfile: %s\n", inputfile);
+        printf("inputdset: %s\n", inputdset);
+        printf("gridsize: %d\n", M);
+        printf("method: %d\n", method);
+        if (restart) {
+            printf("restartfile: %s\n", restartfile);
+            printf("restartdset: %s\n", restartdset);
+        }
+    }
+
+    hsize_t Nloc = M / size;
+    hsize_t offset = rank * Nloc;
+    rank == size - 1 ? Nloc += M % size : Nloc;
+
+    /* select method */
     void (*update)(std::vector<float> &, std::vector<float> &,
                    std::vector<float> &, std::vector<float> &, hsize_t,
                    const hssize_t);
@@ -78,6 +90,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    /* read input */
     std::vector<float> f(Nloc * M * M);
     read1D(f, inputfile, inputdset, Nloc, offset, M);
 
@@ -95,6 +108,10 @@ int main(int argc, char **argv) {
     int iter = 0;
     float res = 1e10;
 
+    /* output file and dataset */
+    char outfname[100] = "phi.h5";
+    char outdset[100] = "phi";
+
     /* create the residual file */
     FILE *fp;
     char fname[100] = "residual.csv";
@@ -102,23 +119,22 @@ int main(int argc, char **argv) {
     fprintf(fp, "iter,residual\n");
     fclose(fp);
 
+    /* main loop */
     while (res > tol && iter < max_iter) {
         update(f, phi, left, right, Nloc, M);
         res = residual(f, phi, left, right, Nloc, M);
         exchange(phi, left, right, Nloc, M);
         iter++;
         if (!rank) printf("iter: %d, residual: %f\n", iter, res);
-        if (iter % dump_interval == 0) {
-            char fname[100] = "phi.h5";
-            char dset[100] = "phi";
-            write1D(phi, fname, dset, Nloc, offset, M);
-        }
+        if (iter % dump_interval == 0)
+            write1D(phi, outfname, outdset, Nloc, offset, M);
         if (!rank) {
             fp = fopen(fname, "a");
             fprintf(fp, "%d,%f\n", iter, res);
             fclose(fp);
         }
     }
+    write1D(phi, outfname, outdset, Nloc, offset, M);
 
     MPI_Finalize();
     return 0;
