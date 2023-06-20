2b913af360b2bb9b6b0ba1b2d990751af3bc9731
lukascodispoti <lukas.codispoti@gmail.com>
2023-06-20 15:17:04 +0200

    fixed convergence curve plotting script

--- convergence-curve.py
+++ convergence-curve.py
@@ -2,12 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-sor = pd.read_csv('residual-sor.csv', names=['iter', 'res'])
-gs = pd.read_csv('residual-gs.csv', names=['iter', 'res'])
-jac = pd.read_csv('residual-jacobi.csv', names=['iter', 'res'])
-
-fix, ax = plt.subplots(1, 1)
 files = sys.argv[1:]
+fix, ax = plt.subplots(1, 1)
 for f in files:
     df = pd.read_csv(f, names=['iter', 'res'])
     ax.plot(df['iter'], df['res'], label=f)
@@ -15,4 +11,4 @@ ax.legend()
 ax.set_xlabel('iteration')
 ax.set_ylabel('residual')
 ax.set_yscale('log')
-plt.savefig('convergence-plot.pdf')
+plt.savefig('convergence-plot.png', dpi=500)
